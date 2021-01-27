c51d03eb10ecc9318e7caa2e39ae652ea73ad521
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-01-27 21:16:04 +0100

    Update BigCalcProg.java

--- src/BigCalcProg.java
+++ src/BigCalcProg.java
@@ -10,14 +10,14 @@ public class BigCalcProg {
     public static void main(String[] args) {  
         try {
         	
-           final CharStream input = CharStreams.fromFileName(args[0]);			// takes file as input
+           final CharStream input = CharStreams.fromFileName(args[0]);		// takes file as input
            final BigCalcProgLexer lexer = new BigCalcProgLexer(input);		// creates lexer
-           final CommonTokenStream tokens = new CommonTokenStream(lexer);		// lexer creates token
+           final CommonTokenStream tokens = new CommonTokenStream(lexer);	// lexer creates token
            final BigCalcProgParser parser = new BigCalcProgParser(tokens);	// parser from token
            final ParseTree tree = parser.root();								// parsetree starting at the root
 
            final BigCalcProgVisitor<BigDecimal> visitor = new BigCalcProgVisitorImpl();	// create visitor from own implementation, processing BigDecimal Objects
-           final BigDecimal result = visitor.visit(tree);									// result is return of root (which visits whole tree)
+           final BigDecimal result = visitor.visit(tree);								// result is return of root (which visits whole tree)
 
            if (result != null) System.out.println("result: " + result.setScale(10, RoundingMode.HALF_UP));
            
